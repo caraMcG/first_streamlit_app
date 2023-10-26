2cbdcc7303072d642808eda6093184c367ec1109
Cara McGinley <43040862+caraMcG@users.noreply.github.com>
2023-10-26 10:59:36 -0600

    Update streamlit_app.py
    added line to show selection in dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #select list to pick fruit user wants included
 streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display table on page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
