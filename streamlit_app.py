0a369c7ab1a0f6f9a1e75f34cd7222be289814e8
Cara McGinley <43040862+caraMcG@users.noreply.github.com>
2023-10-26 11:00:31 -0600

    Update streamlit_app.py
    corrected a variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #select list to pick fruit user wants included
-streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display table on page
