85c7da1599ae3cd59ee309f46c8807216bedf250
Cara McGinley <43040862+caraMcG@users.noreply.github.com>
2023-10-27 15:00:12 -0600

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header("View Our Fruit List - Add Your Favorites")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
